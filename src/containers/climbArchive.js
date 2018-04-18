d31edfa28d0e6d95865f26b3113f81a21f32d064
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2018-04-18 14:49:26 +0100

    initial commit -- refactored from core

---
+++ src/containers/climbArchive.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { fetchSessionGraph, scorePrevPageStatic, scoreNextPageStatic, postNextPageAnnotation, transitionToSession, resetNextSessionTrigger } from 'meld-clients-core/src/actions/index';
+import { connect } from 'react-redux' ;
+import { bindActionCreators } from 'redux';
+import { withRouter } from 'react-router';
+import Score from 'meld-clients-core/src/containers/score';
+import AnnotationsListing from 'meld-clients-core/src/containers/annotationsListing';
+import { setScoreReducerVerovioOptions } from  'meld-clients-core/src/reducers/reducer_score';
+
+class ClimbArchive extends Component { 
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() { 
+		const graphUri = this.props.location.query.session;
+		this.props.fetchSessionGraph(graphUri);
+		setScoreReducerVerovioOptions({
+				ignoreLayout: 1,
+				adjustPageHeight:1,
+				scale:35,
+				pageHeight: 1000*100/35,
+				pageWidth: 700*100/35
+		});
+	}
+	
+	render() { 
+		if(this.props.score.publishedScores) {
+			let session = "";
+			let etag = "";
+			if (this.props.graph && this.props.graph.annoGraph) { 
+				session = this.props.graph.annoGraph["@id"];
+				etag = this.props.graph.etags[session];
+			}
+			const byId = this.props.graph.targetsById;
+			const publishedScores = this.props.score.publishedScores;
+			const conceptualScores = this.props.score.conceptualScores;
+			const scores = Object.keys(publishedScores).map((pS) => {
+				//return <Score key={ sc } uri={ sc } annotations={ byId[sc]["annotations"] } />;
+				const cS = publishedScores[pS];
+				const annotationTargets = conceptualScores[cS];
+				annotations = Object.keys(byId).map((t) => {
+					if(annotationTargets && annotationTargets.includes(t)) { 
+						return byId[t].annotations
+					}
+				});
+				annotations = annotations.reduce( (a, b) => a.concat(b), []);
+				return (
+					<div key={ "wrapper" + pS } >
+						 <AnnotationsListing annotations={ annotations } scoreUri={pS}/>
+						 <Score key={ pS } uri={ pS } annotations={ annotations } session={ session } etag={ etag } nextSession = { this.props.nextSession } />
+					
+						<div id="prev" key={ "prev"+pS } onClick={() => {
+							console.log("prev clicked, ps: ", pS, this.props.score.pageNum, this.props.score.MEI);
+							this.props.scorePrevPageStatic(pS, this.props.score.pageNum, this.props.score.MEI[pS])
+						}}> Previous </div>
+						<div id="next" key={ "next"+pS } onClick={() => {
+							console.log("next clicked, ps: ", pS, this.props.score.pageNum, this.props.score.MEI);
+							//this.props.scoreNextPage(pS, this.props.score.pageNum, this.props.score.MEI[pS])
+							this.props.scoreNextPageStatic(pS, this.props.score.pageNum, this.props.score.MEI[pS])
+						}}> Next </div>
+					</div>
+				)
+			});
+			return (
+				<div>
+					<link rel="stylesheet" href="../../style/climbArchive.css" type="text/css" />
+					<div id="annotations"></div>
+					{ scores }
+				</div>
+			)
+		}
+		return (<div>Loading...</div>);
+	}
+	
+};
+
+function mapStateToProps({ graph, score }) {
+	return { graph, score }
+}
+
+function mapDispatchToProps(dispatch) { 
+	return bindActionCreators({ fetchSessionGraph, scorePrevPageStatic, scoreNextPageStatic, postNextPageAnnotation, transitionToSession, resetNextSessionTrigger }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClimbArchive);
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React, { Component }  from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import ReduxPromise from 'redux-promise';
+import { Router, Route, browserHistory } from 'react-router'
+
+import reducers from 'meld-clients-core/src/reducers';
+import ClimbArchive from './containers/climbArchive';
+
+const createStoreWithMiddleware = applyMiddleware(thunk, ReduxPromise)(createStore);
+
+console.log("Reducers: ", reducers)
+
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<Router history={browserHistory}> 
+			<Route path="/archive" component={ClimbArchive} />
+		</Router>
+	</Provider>
+		, document.querySelector('.container'));
