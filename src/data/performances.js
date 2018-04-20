35b12f2cdadcc72c76faa7e835b578f8b1b4e2bd
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2018-04-20 15:27:58 +0100

    add performances data

---
+++ src/data/performances.js
@@ -0,0 +1,58 @@
+export const performances = {
+	"20180119-allyourbass1": {
+		"basecamp": "http://127.0.0.1:5000/sessions/VMqv3TLthsZ3oLqrtmas6N",
+		"1b": "http://127.0.0.1:5000/sessions/8qJvVCxYZZX6BQVD8mfkE6",
+		"1c": "http://127.0.0.1:5000/sessions/du87PFNWi5CqebsfETsSbn",
+		"p3a": "http://127.0.0.1:5000/sessions/XZYcFsqnecPLByYDwT9seD",
+		"2c": "http://127.0.0.1:5000/sessions/dKohPEL2HUrDF568ePP9zg",
+		"2a": "http://127.0.0.1:5000/sessions/zCNCt7FhdeS8K8Tm9NLUjW",
+		"p1b":  "http://127.0.0.1:5000/sessions/3R3V3rgGkjN9pA44dKiZY6",
+		"3a": "http://127.0.0.1:5000/sessions/jkvqQZptRZbyY2AkHmNS73",
+		"p1c": "http://127.0.0.1:5000/sessions/rLLVbkYhhACwz7yqB4QnyE",
+		"4a": "http://127.0.0.1:5000/sessions/MJKKDsGGG6HcaXtbcueABZ",
+		"5a": "http://127.0.0.1:5000/sessions/M6uRVB4DnFwmHdANcSZ9Ho",
+		"summit": "http://127.0.0.1:5000/sessions/SRBxgVUx4Gxv4rYFBtNd7a"
+	},
+	"20180119-allyourbass2": { 
+		"basecamp":"http://127.0.0.1:5000/sessions/5r3iKaF64mhxWvfUoA8rYM",
+		"1b": "http://127.0.0.1:5000/sessions/kGq78cb7CLHTKpQy7F3Zo4",
+		"1c": "http://127.0.0.1:5000/sessions/LNaPRF4chbvS4VtWQqa5H2",
+		"p3a": "http://127.0.0.1:5000/sessions/6eAL8WDgf2GbDryg9SaEiB",
+		"2c": "http://127.0.0.1:5000/sessions/9YBRHmpBR8qEsUjU88Khwa",
+		"2b": "http://127.0.0.1:5000/sessions/HNMyi393UEw7EX3AEX7dDL",
+		"p2b": "http://127.0.0.1:5000/sessions/oWbz2BjFbVqf6sjyKgbvAF",
+		"3b": "http://127.0.0.1:5000/sessions/QauKaNiEmKWAys2hPMikBn",
+		"p2c": "http://127.0.0.1:5000/sessions/AWveFVhL72Tqtki6cRtPWZ",
+		"4b": "http://127.0.0.1:5000/sessions/5XWfzEacvjvqQV2LtaHszh",
+		"5b": "http://127.0.0.1:5000/sessions/vyco5xUZYnFfftxw7sdVv4",
+		"summit": "http://127.0.0.1:5000/sessions/njQbbCuBa9V7avabcokEr9"
+	}, 
+	"20180221-rcc-1": {
+		"basecamp": "http://127.0.0.1:5000/sessions/Tne7EEfEZ74SZki2aoM8ZR",
+		"1a": "http://127.0.0.1:5000/sessions/LBvnx8FcjMd3sFzjhzjwUh",
+		"2b": "http://127.0.0.1:5000/sessions/3CwNh8AKbxgYZc4kH9Lf3M",
+		"p2b": "http://127.0.0.1:5000/sessions/2xaPPPeKpqjqSfNzQCN8Cn",
+		"3b": "http://127.0.0.1:5000/sessions/aqfHHCZkoXSAgeNhHXgsdW",
+		"3c": "http://127.0.0.1:5000/sessions/nr4jBLj8SAp46uPYjQjqp3",
+		"3a": "http://127.0.0.1:5000/sessions/yxy7QinnWS2Ct2ndyk7d8Z",
+		"p1c":"http://127.0.0.1:5000/sessions/oysY6hyoRDFakcdw4Zbifm",
+		"4a": "http://127.0.0.1:5000/sessions/2K5FtdionFGNhi9GPagtKd",
+		"5a": "http://127.0.0.1:5000/sessions/88QqyVTjgKgcXkvrJbnoqc",
+		"5b": "http://127.0.0.1:5000/sessions/JFtuLiUEsNhkw5YFGKih4F",
+		"5c": "http://127.0.0.1:5000/sessions/cRfockmwJCFsrd6U6QFyXW",
+		"summit": "http://127.0.0.1:5000/sessions/geotby46wmciPY4Rx5Vyni"
+	},
+	"20180221-rcc-2": {
+		"basecamp": "http://127.0.0.1:5000/sessions/FtuBBKYVyayu9YuhvxZCwL",
+		"1c":"http://127.0.0.1:5000/sessions/Av6KjnNiekt9x6JqvY4zLL",
+		"p3a": "http://127.0.0.1:5000/sessions/RTWGsQSTZNHL8pdocuhJwF",
+		"2c": "http://127.0.0.1:5000/sessions/vBsmxYJrtjEHLKsizG7qT8",
+		"2a": "http://127.0.0.1:5000/sessions/83VSmeA4wSroAuETCHHM3E",
+		"p1b": "http://127.0.0.1:5000/sessions/5Udc5e8JLGSDHAFXJi9Vnn",
+		"3a": "http://127.0.0.1:5000/sessions/EAaHCtKwHoYECG7EaAGLvL",
+		"p1c": "http://127.0.0.1:5000/sessions/ZdYp5wgKYw5cKe6XMvD56A",
+		"4a": "http://127.0.0.1:5000/sessions/NRoVJCapZnzynxJ6hpw8eB",
+		"5a": "http://127.0.0.1:5000/sessions/RLZctpirNGi4oUFNYyesFd",
+		"summit": "http://127.0.0.1:5000/sessions/MtUqxnnfXuQ3AxQenZRGn9"
+	}
+}
