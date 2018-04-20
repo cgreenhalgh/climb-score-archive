a43a21decef6cd50370bcb9fd8a7155329a5198f
David M. Weigl <david.weigl@oerc.ox.ac.uk>
2018-04-20 16:43:41 +0100

    Sanity check if we have a session for the perf / frag combination, if not display 'awaiting performance selection'

--- src/containers/climbArchive.js
+++ src/containers/climbArchive.js
@@ -33,50 +33,52 @@ class ClimbArchive extends Component {
 	render() { 
 		if(this.props.location.query.perf && 
 			this.props.score.publishedScores) {
-			let session = "";
-			let etag = "";
-			if (this.props.graph && this.props.graph.annoGraph) { 
-				session = this.props.graph.annoGraph["@id"];
-				etag = this.props.graph.etags[session];
+				let session = "";
+				let etag = "";
+				if (this.props.graph && this.props.graph.annoGraph) { 
+					session = this.props.graph.annoGraph["@id"];
+					etag = this.props.graph.etags[session];
+				}
+				if(session) { 
+					const byId = this.props.graph.targetsById;
+					const publishedScores = this.props.score.publishedScores;
+					const conceptualScores = this.props.score.conceptualScores;
+					const scores = Object.keys(publishedScores).map((pS) => {
+						//return <Score key={ sc } uri={ sc } annotations={ byId[sc]["annotations"] } />;
+						const cS = publishedScores[pS];
+						const annotationTargets = conceptualScores[cS];
+						annotations = Object.keys(byId).map((t) => {
+							if(annotationTargets && annotationTargets.includes(t)) { 
+								return byId[t].annotations
+							}
+						});
+						annotations = annotations.reduce( (a, b) => a.concat(b), []);
+						return (
+							<div key={ "wrapper" + pS } >
+								 <AnnotationsListing annotations={ annotations } scoreUri={pS}/>
+								 <Score key={ pS } uri={ pS } annotations={ annotations } session={ session } etag={ etag } nextSession = { this.props.nextSession } />
+							
+								<div id="prev" key={ "prev"+pS } onClick={() => {
+									console.log("prev clicked, ps: ", pS, this.props.score.pageNum, this.props.score.MEI);
+									this.props.scorePrevPageStatic(pS, this.props.score.pageNum, this.props.score.MEI[pS])
+								}}> Previous </div>
+								<div id="next" key={ "next"+pS } onClick={() => {
+									console.log("next clicked, ps: ", pS, this.props.score.pageNum, this.props.score.MEI);
+									//this.props.scoreNextPage(pS, this.props.score.pageNum, this.props.score.MEI[pS])
+									this.props.scoreNextPageStatic(pS, this.props.score.pageNum, this.props.score.MEI[pS])
+								}}> Next </div>
+							</div>
+						)
+					});
+					return (
+						<div>
+							<link rel="stylesheet" href="../../style/climbArchive.css" type="text/css" />
+							<div id="annotations"></div>
+							{ scores }
+						</div>
+					)
+				}
 			}
-			const byId = this.props.graph.targetsById;
-			const publishedScores = this.props.score.publishedScores;
-			const conceptualScores = this.props.score.conceptualScores;
-			const scores = Object.keys(publishedScores).map((pS) => {
-				//return <Score key={ sc } uri={ sc } annotations={ byId[sc]["annotations"] } />;
-				const cS = publishedScores[pS];
-				const annotationTargets = conceptualScores[cS];
-				annotations = Object.keys(byId).map((t) => {
-					if(annotationTargets && annotationTargets.includes(t)) { 
-						return byId[t].annotations
-					}
-				});
-				annotations = annotations.reduce( (a, b) => a.concat(b), []);
-				return (
-					<div key={ "wrapper" + pS } >
-						 <AnnotationsListing annotations={ annotations } scoreUri={pS}/>
-						 <Score key={ pS } uri={ pS } annotations={ annotations } session={ session } etag={ etag } nextSession = { this.props.nextSession } />
-					
-						<div id="prev" key={ "prev"+pS } onClick={() => {
-							console.log("prev clicked, ps: ", pS, this.props.score.pageNum, this.props.score.MEI);
-							this.props.scorePrevPageStatic(pS, this.props.score.pageNum, this.props.score.MEI[pS])
-						}}> Previous </div>
-						<div id="next" key={ "next"+pS } onClick={() => {
-							console.log("next clicked, ps: ", pS, this.props.score.pageNum, this.props.score.MEI);
-							//this.props.scoreNextPage(pS, this.props.score.pageNum, this.props.score.MEI[pS])
-							this.props.scoreNextPageStatic(pS, this.props.score.pageNum, this.props.score.MEI[pS])
-						}}> Next </div>
-					</div>
-				)
-			});
-			return (
-				<div>
-					<link rel="stylesheet" href="../../style/climbArchive.css" type="text/css" />
-					<div id="annotations"></div>
-					{ scores }
-				</div>
-			)
-		}
 		return (<div>Awaiting performance selection...</div>);
 	}
 	
